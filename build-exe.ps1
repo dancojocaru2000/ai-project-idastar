7fdd5032d57919da684250d967b5eae15982fc3c
Dan Cojocaru <dancojocaru2010@hotmail.com>
2021-12-04 14:53:20 +0200

    Added build script

---
+++ build-exe.ps1
@@ -0,0 +1,31 @@
+#! /usr/bin/env pwsh
+
+$options = @("&Windows", "&Linux")
+$choice = $host.UI.PromptForChoice(
+	"Compile exe", 
+	"What OS to compile the executable for?",
+	$options,
+	-1
+)
+
+$platform = $null
+switch ($choice) {
+	0 { $platform = "win-x64" }
+	1 { $platform = "linux-x64" }
+	Default {
+		Write-Host "No OS chosen, exiting."
+		Exit
+	}
+}
+
+$sc = $null
+switch ($host.UI.PromptForChoice(
+	"Self Contained", 
+	"Should the executable be runnable without installing .NET Runtime?",
+	@("&Yes", "&No"),
+	0)) {
+	0 { $sc = "true" }
+	1 { $sc = "false" }
+}
+
+dotnet publish -r $platform --self-contained $sc
